392f2f47bbe03fe4e8d81d12a371761a695ad481
kishan <kihdg@gmail.com>
2022-11-18 15:27:49 +0530

    second commit

---
+++ lib/Screen/new_Screen.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class NewScreen extends StatelessWidget {
+  const NewScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
